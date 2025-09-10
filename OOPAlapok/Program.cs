61d7eb7383a779c97603cd422bcee30439ec1797
FZoltan40 <farkasz@kkszki.hu>
2025-09-10 08:30:51 +0200

    Feladat1 is ready.

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace OOPAlapok
 {
+    public class Szemely
+    {
+        public string nev = "Peti";
+        public int kor=34;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine($"A tanulo neve: {tanulo1.nev} és {tanulo1.kor} éves.");
         }
     }
 }
