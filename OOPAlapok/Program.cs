2c6885cd27e8358f6e39234591237f00819fde39
FZoltan40 <farkasz@kkszki.hu>
2025-09-10 09:10:16 +0200

    Feladat2 is ready.

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -8,15 +8,28 @@ namespace OOPAlapok
 {
     public class Szemely
     {
-        public string nev = "Peti";
-        public int kor=34;
+        private string nev;
+        private int kor;
+
+        public Szemely(string Nev,int Kor)
+        {
+            nev = Nev;
+            kor = Kor;
+        }
+
+        public string Kiir()
+        {
+            return $"A tanulo neve: {nev} és {kor} éves.";
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely tanulo1 = new Szemely();
-            Console.WriteLine($"A tanulo neve: {tanulo1.nev} és {tanulo1.kor} éves.");
+            Szemely tanulo1 = new Szemely("Jani",55);
+            Console.WriteLine(tanulo1.Kiir());
+            Szemely tanulo2 = new Szemely("Ildi", 25);
+            Console.WriteLine(tanulo2.Kiir());
         }
     }
 }
